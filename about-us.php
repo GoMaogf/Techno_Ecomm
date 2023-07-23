a255a3a332ad4833135a27ef1a495a0f9006f3e0
John Bruce R. Balangatan <60367242+JohnBruce626@users.noreply.github.com>
2023-07-24 03:21:03 +0800

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,68 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-yellow layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+
+	      <!-- Main content -->
+	      <section class="content">
+	        <style>
+    .About-us {
+	  width: 100%;
+	  height:auto;
+	  text-align: center;
+	  background-color: #f39c12;
+	  margin-top: 30px;
+	  margin-bottom: 30px;
+	  border: 5px solid #000000;
+	  border-radius: 10px 10px 10px 10px;
+	}
+	h1{
+	  color: #ffffff;
+	  font-size: 35px;
+	}
+	img {
+	  border-radius: 50%;
+	}
+	
+	.p1 {
+		color: #ffffff;
+	  font-size: 20px;
+	  text-align: center;
+	}
+	
+</style>
+</head>
+<body>
+  <div class="About-us">
+    <h1> About Us </h1>	
+	<img src="images/logo.png">
+	<p class="p1">Welcome to Eygi Yu-Gi-Oh, the best place to find everything about Yu-Gi-Oh! We are an online marketplace with a big range of Yu-Gi-Oh trading cards, accessories, and other products for collectors and players who love the game.</p>
+	<hr>
+	<p class="p1">   At Eygi Yu-Gi-Oh, we know how exciting it is to battle and how fun it is to build your perfect deck. So, we collect a wide range of cards, from rare and hard-to-find collectibles to the newest booster packs, so you can always find what you're looking for to improve your gaming experience.  <br><br>
+
+        Our website is easy to use, so looking around and buying cards is simple. Eygi Yu-Gi-Oh has something for everyone, whether you are a seasoned player, a casual player, or a serious collector.<br><br>
+
+        We put customer happiness first and work hard to give great service. Our team of committed Yu-Gi-Oh fans is always ready to help you with any questions or problems you might have. We offer safe ways to pay and fast shipping so your purchases get to you quickly and safely.</p>
+   
+  </div>
+</body>
+</html>
+
+	        	</div>
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
