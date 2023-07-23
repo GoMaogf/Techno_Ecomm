bdc6b43ac4841ca33bf455b12ea5ffc5942dea13
John Bruce R. Balangatan <60367242+JohnBruce626@users.noreply.github.com>
2023-07-24 03:42:44 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Card Trading Shop</b>
+      </div>
+      <strong>EyGi Yu-Gi-Oh!</a></strong>
+    </div>
+</footer> 
